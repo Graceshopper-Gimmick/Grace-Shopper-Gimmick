4d051f249569bfe7a0ef3590109b2e8ea3373a38
MonilGoklani <monil2912@gmail.com>
2021-02-13 18:00:14 -0500

    added logic to sort products by id

--- fs-app-template/server/api/products.js
+++ fs-app-template/server/api/products.js
@@ -4,12 +4,11 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Product.findAll(
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      //attributes: ['id','name', 'price', 'thumbnailImgUrl', 'ogImgUrl']
-    )
+    const products = await Product.findAll({
+      order:[
+        ['id','ASC']
+      ]
+    })
     res.send(products)
   } catch (err) {
     next(err)
